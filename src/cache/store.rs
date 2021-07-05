bbedae2fea34eb915e55ace85aed20733579cffa
Jeff Rade <jeffrade@gmail.com>
2021-07-05 12:01:36 -0500

    Add a registerd users cache for password files

--- src/cache/store.rs
+++ src/cache/store.rs
@@ -9,18 +9,33 @@ lazy_static! {
         let map = HashMap::new();
         Mutex::new(map)
     };
-    static ref USERS: Mutex<HashMap<&'static str, Vec<u8>>> = {
+    static ref USERS: Mutex<HashMap<String, Vec<u8>>> = {
         let map = HashMap::new();
         Mutex::new(map)
     };
 }
 
-pub fn insert(k: u32, v: Vec<u8>) -> bool {
+pub fn insert(k: u32, v: Vec<u8>) {
     let mut cache = CACHE.lock().unwrap();
-    cache.insert(k, v).is_some()
+    cache.insert(k, v);
 }
 
 pub fn get(k: &u32) -> Option<Vec<u8>> {
     let cache = CACHE.lock().unwrap();
     cache.get(k).cloned()
+}
+
+pub fn add_user(user_name: String, password_file: Vec<u8>) {
+    let mut cache = USERS.lock().unwrap();
+    cache.insert(user_name, password_file);
+}
+
+pub fn get_user(user_name: &str) -> Option<Vec<u8>> {
+    let cache = USERS.lock().unwrap();
+    cache.get(user_name).cloned()
+}
+
+pub fn user_exists(user_name: &str) -> bool {
+    let cache = USERS.lock().unwrap();
+    cache.contains_key(user_name)
 }
