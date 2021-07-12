734ba8fe00e5779a99af46b1a269cf6045ba9c16
Jeff Rade <jeffrade@gmail.com>
2021-07-12 12:53:32 -0500

    Move generic directory creation logic into util module

--- src/crypto/init.rs
+++ src/crypto/init.rs
@@ -1,18 +1,6 @@
-use std::fs;
-
 use crate::util;
 
 pub fn init() -> Result<(), std::io::Error> {
     let app_dir = util::default_dir();
-    match fs::read_dir(&app_dir) {
-        Ok(_) => Ok(()),
-        Err(err) => {
-            println!(
-                "WARNING: Looked for directory {}. But error {:?}",
-                &app_dir, err
-            );
-            println!("INFO: Will attempt to create directory {}", &app_dir);
-            fs::create_dir(&app_dir)
-        }
-    }
+    util::create_directory(&app_dir)
 }
--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 pub fn default_dir() -> String {
     String::from(env!("HOME")) + "/.keypost-app"
@@ -8,5 +9,16 @@ pub fn get_env_var(var: &str, default: &str) -> String {
     match env::var(var) {
         Ok(value) => value,
         Err(_e) => String::from(default),
+    }
+}
+
+pub fn create_directory(dir: &str) -> Result<(), std::io::Error> {
+    match fs::read_dir(dir) {
+        Ok(_) => Ok(()),
+        Err(err) => {
+            println!("WARNING: Looked for directory {}. But error {:?}", dir, err);
+            println!("INFO: Will attempt to create directory {}", dir);
+            fs::create_dir(dir)
+        }
     }
 }
