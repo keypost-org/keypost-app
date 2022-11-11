c7780b0acf47806404980ff547c37d65635bad40
Jeff Rade <jeffrade@gmail.com>
2022-11-10 18:55:34 -0600

    Rename to DefaultCipherSuite struct for CipherSuite trait and match opaque-ke lib

--- src/crypto/opaque.rs
+++ src/crypto/opaque.rs
@@ -10,12 +10,12 @@ use opaque_ke::{
 use crate::util;
 
 lazy_static! {
-    static ref SERVER_SETUP: Mutex<ServerSetup<Default>> = {
+    static ref SERVER_SETUP: Mutex<ServerSetup<DefaultCipherSuite>> = {
         let server_setup_location = util::default_dir() + "/server_setup.private";
         let server_setup = match util::read_file(&server_setup_location) {
             Ok(bytes) => {
                 println!("DEBUG: Found server_setup file");
-                ServerSetup::<Default>::deserialize(&bytes).unwrap_or_else(|err| {
+                ServerSetup::<DefaultCipherSuite>::deserialize(&bytes).unwrap_or_else(|err| {
                     println!("ERROR: {:?}", err);
                     panic!(
                         "Could not deserialize bytes from file {}",
@@ -26,7 +26,7 @@ lazy_static! {
             Err(err) => {
                 println!("DEBUG: Could not find server_setup file - error: {:?}", err);
                 let mut server_rng = OsRng;
-                let server_setup = ServerSetup::<Default>::new(&mut server_rng);
+                let server_setup = ServerSetup::<DefaultCipherSuite>::new(&mut server_rng);
                 util::write_to_file(&server_setup_location, &server_setup.serialize())
                     .unwrap_or_else(|err| {
                         println!("ERROR: {:?}", err);
@@ -44,10 +44,10 @@ lazy_static! {
 
 // The CipherSuite trait allows to specify the underlying primitives
 // that will be used in the OPAQUE protocol
-pub struct Default;
+pub struct DefaultCipherSuite;
 
 #[cfg(feature = "ristretto255")]
-impl CipherSuite for Default {
+impl CipherSuite for DefaultCipherSuite {
     type OprfCs = opaque_ke::Ristretto255;
     type KeGroup = opaque_ke::Ristretto255;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
@@ -55,7 +55,7 @@ impl CipherSuite for Default {
 }
 
 #[cfg(not(feature = "ristretto255"))]
-impl CipherSuite for Default {
+impl CipherSuite for DefaultCipherSuite {
     type OprfCs = p256::NistP256;
     type KeGroup = p256::NistP256;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
@@ -73,11 +73,11 @@ impl Opaque {
         &self,
         registration_request_base64: &str,
         email: &str,
-    ) -> ServerRegistrationStartResult<Default> {
+    ) -> ServerRegistrationStartResult<DefaultCipherSuite> {
         let registration_request_bytes =
             base64::decode(registration_request_base64).expect("Could not perform base64 decode");
         let server_setup = SERVER_SETUP.lock().unwrap(); // FIXME https://doc.rust-lang.org/stable/std/sync/struct.Mutex.html#poisoning
-        ServerRegistration::<Default>::start(
+        ServerRegistration::<DefaultCipherSuite>::start(
             &server_setup,
             RegistrationRequest::deserialize(&registration_request_bytes[..]).unwrap(),
             email.as_bytes(),
@@ -89,7 +89,8 @@ impl Opaque {
         let client_message_bytes =
             base64::decode(client_message_base64).expect("Could not perform base64 decode");
         let password_file = ServerRegistration::finish(
-            RegistrationUpload::<Default>::deserialize(&client_message_bytes[..]).unwrap(),
+            RegistrationUpload::<DefaultCipherSuite>::deserialize(&client_message_bytes[..])
+                .unwrap(),
         );
         password_file.serialize().to_vec()
     }
@@ -99,11 +100,11 @@ impl Opaque {
         email: &str,
         password_file_bytes: &[u8],
         credential_request_base64: &str,
-    ) -> ServerLoginStartResult<Default> {
+    ) -> ServerLoginStartResult<DefaultCipherSuite> {
         let credential_request_bytes =
             base64::decode(credential_request_base64).expect("Could not perform base64 decode");
         let password_file =
-            ServerRegistration::<Default>::deserialize(password_file_bytes).unwrap();
+            ServerRegistration::<DefaultCipherSuite>::deserialize(password_file_bytes).unwrap();
         let mut server_rng = OsRng;
         let server_setup = SERVER_SETUP.lock().unwrap();
         ServerLogin::start(
@@ -130,7 +131,8 @@ impl Opaque {
     ) -> Result<(), ProtocolError> {
         let credential_finalization_bytes = base64::decode(credential_finalization_base64)
             .expect("Could not perform base64 deocde");
-        let server_login = ServerLogin::<Default>::deserialize(server_login_bytes).unwrap();
+        let server_login =
+            ServerLogin::<DefaultCipherSuite>::deserialize(server_login_bytes).unwrap();
         server_login.finish(CredentialFinalization::deserialize(
             &credential_finalization_bytes[..],
         )?)?;
